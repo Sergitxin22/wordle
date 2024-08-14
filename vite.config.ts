bb31e2839bbb0cdcde21902764cf3c70761041c6
Sergitxin <sergio.morales@opendeusto.es>
2024-08-14 22:03:36 +0200

    chore: Update Vite configuration for relative paths and build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Esto asegura que se usen rutas relativas
+  build: {
+    outDir: 'docs', // Configura la salida del build en la carpeta 'docs'
+  },
 })
