6f532f97cf8c0f5dd011bf92b2a02c809d7ec2c4
Sergitxin <sergio.morales@opendeusto.es>
2024-08-27 18:35:45 +0200

    chore: Add Header component for better UI navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Board from './components/Board'
 import Keyboard from './components/Keyboard'
 import { boardDefault, generateWordSet } from './Words';
 import GameOver from './components/GameOver';
+import Header from './components/Header';
 
 export const AppContext = createContext();
 
@@ -14,7 +15,7 @@ function App() {
   const [disabledLetters, setDisabledLetters] = useState([]);
   const [correctWord, setCorrectWord] = useState('');
   const [wordDefinitions, setWordDefinitions] = useState([]);
-  const [gameOver, setGameOver] = useState({gameOver: false, guessedWord: false});
+  const [gameOver, setGameOver] = useState({ gameOver: false, guessedWord: false });
 
   useEffect(() => {
     generateWordSet().then((words) => {
@@ -28,7 +29,7 @@ function App() {
 
   const onSelectLetter = (keyVal) => {
     if (currAttempt.letterPos > 4) return
-  
+
     const newBoard = [...board]
     newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal
     setBoard(newBoard)
@@ -54,54 +55,57 @@ function App() {
 
     // console.log(currWord);
     // console.log(wordSet);
-    
+
     // console.log({wordSet, currWord});
-    if (wordSet.has(currWord.toLowerCase())) {      
+    if (wordSet.has(currWord.toLowerCase())) {
       setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 })
     } else {
       alert('La palabra no existe')
     }
 
     if (currWord === correctWord) {
-      setGameOver({gameOver: true, guessedWord: true})
+      setGameOver({ gameOver: true, guessedWord: true })
       return
     }
 
     if (currAttempt.attempt === 5) {
-      setGameOver({gameOver: true, guessedWord: false})
+      setGameOver({ gameOver: true, guessedWord: false })
       return
     }
   }
 
   return (
-    <div className='App'>
-      <nav>
-        <h1>Wordle</h1>
-      </nav>
-      <AppContext.Provider 
-        value={{ 
-          board,
-          setBoard,
-          currAttempt,
-          setCurrAttempt,
-          onSelectLetter,
-          onDelete,
-          onEnter,
-          correctWord,
-          guessedLetterUsage,
-          setGuessedLetterUsage,
-          disabledLetters,
-          setDisabledLetters,
-          gameOver,
-          setGameOver,
-          wordDefinitions,
-          setWordDefinitions
-        }}>
-        <div className="game">
-          <Board />
-          {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+    <div className="dark">
+      <div className="dark:bg-dark dark:text-neutral-100 text-black">
+        <div className="container mx-auto flex flex-col max-w-md h-screen">
+          <Header />
+          <div className="Toastify"></div>
+          <AppContext.Provider
+            value={{
+              board,
+              setBoard,
+              currAttempt,
+              setCurrAttempt,
+              onSelectLetter,
+              onDelete,
+              onEnter,
+              correctWord,
+              guessedLetterUsage,
+              setGuessedLetterUsage,
+              disabledLetters,
+              setDisabledLetters,
+              gameOver,
+              setGameOver,
+              wordDefinitions,
+              setWordDefinitions
+            }}>
+            <main className="flex flex-auto justify-center items-center">
+              <Board />
+            </main>
+              {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+          </AppContext.Provider>
         </div>
-      </AppContext.Provider>
+      </div>
     </div>
   )
 }
