3ac1fc5048bb7399597505b90c039150372fdada
Sergitxin <sergio.morales@opendeusto.es>
2024-08-27 18:34:49 +0200

    chore: Update Tailwind CSS and PostCSS configurations

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        // 'dark': 'rgb(18 18 19)',
+      },
+      fontSize: {
+        'text-base': '1rem',
+      },
+      
+    }
+  },
+  plugins: [],
+}
