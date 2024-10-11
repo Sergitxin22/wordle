01f1c9a0682304aa78bc9a9fbe9509fc73ffe135
Sergitxin <sergio.morales@opendeusto.es>
2024-10-11 17:59:02 +0200

    Add option's theme control

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundColor: {
@@ -12,7 +13,7 @@ export default {
       fontSize: {
         'text-base': '1rem',
       },
-      
+
     }
   },
   plugins: [],
