74e89e3138163557e170d8611c1d2f3e7f530bca
Sergitxin22 <65020729+Sergitxin22@users.noreply.github.com>
2025-03-01 14:15:29 +0000

    Deploying to gh-pages from @ Sergitxin22/wordle@cc0e749bf42ba3bfb1e194671578c4071f11fa03 🚀

---
+++ env-config.js
@@ -0,0 +1,9 @@
+// This file will be loaded before the application and will provide environment variables
+window.__ENV__ = {
+  VITE_FIREBASE_API_KEY: 'AIzaSyAkHV6vrSil2EJp1B8ng6YGvSwkhTwpN8Y',
+  VITE_FIREBASE_AUTH_DOMAIN: 'wordle-d6fe7.firebaseapp.com',
+  VITE_FIREBASE_PROJECT_ID: 'wordle-d6fe7',
+  VITE_FIREBASE_STORAGE_BUCKET: 'wordle-d6fe7.firebasestorage.app',
+  VITE_FIREBASE_MESSAGING_SENDER_ID: '1078610634641',
+  VITE_FIREBASE_APP_ID: '1:1078610634641:web:a0c5cf820a4b46250f1c30',
+};
